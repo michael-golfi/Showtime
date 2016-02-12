73f83fee7ea8cb49001961ca00030002209ac1ba
Simon Lei <name.simonlei@gmail.com>
2016-02-12 16:53:06 -0500

    Fixed faulty test for suggested search

--- app/src/androidTest/java/com/example/showtime/app/MovieListActivityTest.java
+++ app/src/androidTest/java/com/example/showtime/app/MovieListActivityTest.java
@@ -1,6 +1,5 @@
 package com.example.showtime.app;
 
-import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -11,24 +10,30 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MovieListActivityTest {
 
     @Rule
-    public ActivityTestRule<MovieListActivity> mActivityRule = new ActivityTestRule(MovieListActivity.class);
+    public ActivityTestRule<MovieListActivity> mActivityRule = new ActivityTestRule(
+            MovieListActivity.class);
 
     @Test
     public void suggestionsOnKeystroke() {
-        onView(withId(R.id.action_search)).check(ViewAssertions.matches(isDisplayed()));
+        onView(withId(R.id.action_search)).check(matches(isDisplayed()));
         onView(withId(R.id.action_search)).perform(click());
         onView(withId(R.id.search_src_text)).perform(typeText("b"));
-        onView(withId(android.R.layout.simple_list_item_activated_1)).check(ViewAssertions.doesNotExist());
+        onView(withText("Bat Hunter - 2006-01-15")).check(doesNotExist());
         onView(withId(R.id.search_src_text)).perform(typeText("a"));
-        onView(withId(android.R.layout.simple_list_item_activated_1)).check(ViewAssertions.doesNotExist());
-        onView(withId(R.id.search_src_text)).perform(typeText("t")).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText("Bat Hunter - 2006-01-15")).check(doesNotExist());
+        onView(withId(R.id.search_src_text)).perform(typeText("t")).check(matches(isDisplayed()));
+        onView(withText("Bat Hunter - 2006-01-15")).check(matches(isDisplayed()));
+        onView(withId(R.id.search_src_text)).perform(typeText("m"));
+        onView(withText("Bat Hunter - 2006-01-15")).check(doesNotExist());
     }
 }
