5c7952edbf790aedd0bbe459dd0e6a8f43be9636
Nicolas Guzman <Namguz@wpa153026.wireless.mcgill.ca>
2016-03-10 11:01:23 -0500

    fixed notifications

--- app/src/main/java/com/example/showtime/app/MovieDetailActivity.java
+++ app/src/main/java/com/example/showtime/app/MovieDetailActivity.java
@@ -1,6 +1,8 @@
 package com.example.showtime.app;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
@@ -75,9 +77,10 @@ public class MovieDetailActivity extends AppCompatActivity implements Button.OnC
         calendar.set(Calendar.MINUTE, 00);
         calendar.set(Calendar.SECOND, 00);
 
-        //setCalendarTime(calendar);
+        setCalendarTime(calendar);
 
-        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis() + 100, pendingIntent);
+        //alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis() + 100, pendingIntent);
+        CreateNotification();
     }
 
     public void setCalendarTime(Calendar calendar)
@@ -90,6 +93,22 @@ public class MovieDetailActivity extends AppCompatActivity implements Button.OnC
         int day = Integer.parseInt(parts[0]);
 
         calendar.set(year, month, day);
+    }
+
+    public void CreateNotification()
+    {
+        Intent intent = new Intent(this, MovieDetailActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Notification n = new Notification.Builder(this)
+                .setContentTitle("Movie: " + MovieDetailFragment.myMovie.getTitle() + ", will be released Today")
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentIntent(pIntent).build();
+
+        MovieDetailFragment.myMovie.getTitle();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, n);
     }
 
     @Override
